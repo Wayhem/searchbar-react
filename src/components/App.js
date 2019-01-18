b289a0e922df50c455f6478e220c168d43c892ae
Jose Villarroel <oscarclarke9900@gmail.com>
2019-01-18 10:17:38 -0400

    finished search bar and handling input submit, search bar styled already, next task is making app component to handle api fetch to show photos of the search input

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '30px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App;
